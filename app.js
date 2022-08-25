59fc6a1c5f160ec263de5342a763bfdb30ecd615
Enrique Lopez <905011@H2WHK129Q6NW.tld>
2022-08-25 14:42:45 -0500

    finishathome

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 function getData(){
-    var place = document.getElementById("place").value
-    var name = document.getElementById("name").value
-    var verb = document.getElementById("verb").value
-    var adj = document.getElementById("adjetive").value
-    var adverb = document.getElementById("adverb").value
+    var place = document.getElementById("place").value;
+    var name = document.getElementById("name").value;
+    var verb = document.getElementById("verb").value;
+    var adj = document.getElementById("adjetive").value;
+    var adverb = document.getElementById("adverb").value;
 
-    console.log([place, name, verb, adj, adverb])
-    return [ place, name, verb, adj, adverb]
+    
+    document.getElementById("sentence").innerHTML = sentences
+    (verb, adverb, person, adjetive);
+    
+    
+    //console.log([place, name, verb, adj, adverb])
+    //return [place, name, verb, adj, adverb]
 }   
 
 
-function sentences(){
+function sentences(verb, adverb, person, adjetive){
     sentenceArray = [`Today I had to ${verb} ${name} but right in the middle of it ${verb} came with this ${adjetive} book and i wanted to go too ${place}`
     , `insert verb here with name ${verb} ${name} now a ${adjetive} and a ${place}`]
 
     ranN = Math.floor(Math.random() * sentenceArray.length)
 
-    return sentencefArray[ranN];
+    return sentenceArray[ranN];
     
 }
 
 console.log (sentences())
-console.log (verb()) 
