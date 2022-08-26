b616af95ba84c140ee75ce72511ea59911389252
Enrique Lopez <905011@H2WHK1AVQ6NW.tld>
2022-08-26 13:04:54 -0500

    finish

--- app.js
+++ app.js
@@ -7,17 +7,17 @@ function getData(){
 
     
     document.getElementById("sentence").innerHTML = sentences
-    (verb, adverb, person, adjetive);
+    (verb, adverb, name, adjetive, place);
     
     
-    //console.log([place, name, verb, adj, adverb])
-    //return [place, name, verb, adj, adverb]
+    console.log([place, name, verb, adj, adverb])
+    return [place, name, verb, adj, adverb]
 }   
 
 
-function sentences(verb, adverb, person, adjetive){
-    sentenceArray = [`Today I had to ${verb} ${name} but right in the middle of it ${verb} came with this ${adjetive} book and i wanted to go too ${place}`
-    , `insert verb here with name ${verb} ${name} now a ${adjetive} and a ${place}`]
+function sentences(verb, adverb, name, adj, place){
+    sentenceArray = [`Today I had to ${verb} ${name} but right in the middle of it ${verb} came with this ${adj} book and i wanted to go too ${adverb}`
+    , `insert verb here with name ${verb} ${name} now a ${adj} and a ${place}`]
 
     ranN = Math.floor(Math.random() * sentenceArray.length)
 
@@ -26,3 +26,49 @@ function sentences(verb, adverb, person, adjetive){
 }
 
 console.log (sentences())
+
+
+
+//second 
+
+
+var = [1, 2, 4, 9]
+
+var wordArr = [
+    {
+        word: "dog"
+        hint: "company you keep at home"
+    },
+    {
+        word: "place"
+        hint: "were you are anytime"
+    },
+    {
+        word: "tree"
+        hint: "it grouws on the ground"
+    },
+    {
+        word: "computer"
+        hint: "youre using it right now"
+    },
+]
+
+var rN = Math.floor(Math.random() * )
+
+var word = wordArr[rN].word;
+var hint = wordArr[rN].hint;
+
+document.getElementById('word').innerHTML= word;
+document.getElementById('hint').innerHTML = hint;
+
+var splitWord = word.split("");
+
+var emptySplitWord = [];
+
+for (i = 0, i < splitWord.length; i++)_ {
+    emptySplitWord[i] = "_"
+    console.log (emptySplitWord);
+    document.getElementById("word") .innerHTML= emptySplitWord.
+    join("")
+
+}
